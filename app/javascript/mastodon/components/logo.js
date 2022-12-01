d47a7ce751cc43e75764ab125c2c48ae7d138739
maddie lim ✨ <maddievision@gmail.com>
2022-11-30 18:09:11 -0800

    :panda_face: another img update

--- app/javascript/mastodon/components/logo.js
+++ app/javascript/mastodon/components/logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <img src="/logo.png" style={{imageRendering: "pixelated"}} width="302" className='logo' role='img' title="rosie’s garden" alt="🌹 rosie’s garden" />
+  <img src="/logo.png" width="150" title="rosie’s garden" alt="🌹 rosie’s garden" />
 );
 
 export default Logo;
