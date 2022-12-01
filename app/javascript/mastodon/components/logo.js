692d800248c020d9abdf8089e51c0820ad27d918
maddie lim ✨ <maddievision@gmail.com>
2022-11-30 18:00:20 -0800

    :milky_way: logo updates

--- app/javascript/mastodon/components/logo.js
+++ app/javascript/mastodon/components/logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <img src="/logo.png" className='logo' role='img' title="rosie’s garden" alt="🌹 rosie’s garden" />
+  <img src="/logo.png" style="image-rendering: pixelated;" width="302" className='logo' role='img' title="rosie’s garden" alt="🌹 rosie’s garden" />
 );
 
 export default Logo;
